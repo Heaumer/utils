470c42ab62dea067ff5c4253ab891a902ff8be1d
mb <mathieu.bivert@fraudbuster.mobi>
2016-08-01 14:37:53 +0200

    s/\t/    /g

--- ucol.c
+++ ucol.c
@@ -18,7 +18,7 @@
 #define KEEPINDENT 0
 
 enum {
-	Maxcol = 128	/* supplementary columns are discarded */
+    Maxcol = 128    /* supplementary columns are discarded */
 };
 
 static int colsize[Maxcol];
@@ -30,156 +30,156 @@ static int nspace = 1;
 int
 wordlen(char *w)
 {
-	int n;
+    int n;
 
-	for (n = 0;*w != '\0'; w++) {
-		/*
-		 * 0xC0 : 1100 0000
-		 * 0x80 : 1000 0000 (10xx xxxx is continuation prefix)
-		 */
-		if (((*w) & 0xC0) != 0x80)
-			n++;
-	}
+    for (n = 0;*w != '\0'; w++) {
+        /*
+         * 0xC0 : 1100 0000
+         * 0x80 : 1000 0000 (10xx xxxx is continuation prefix)
+         */
+        if (((*w) & 0xC0) != 0x80)
+            n++;
+    }
 
-	return n;
+    return n;
 }
 
 int
 countcols(char *line)
 {
-	char *p;
-	int n;
+    char *p;
+    int n;
 
-	n = 0;
+    n = 0;
 
 #if KEEPINDENT
-	while(isspace(*line))
-		line++;
+    while(isspace(*line))
+        line++;
 #endif
 
-	for (p = strtok(line, SEP); p != NULL && n < Maxcol; p = strtok(NULL, SEP)) {
-		if(wordlen(p) > colsize[n])
-			colsize[n] = wordlen(p);
-		n++;
-	}
+    for (p = strtok(line, SEP); p != NULL && n < Maxcol; p = strtok(NULL, SEP)) {
+        if(wordlen(p) > colsize[n])
+            colsize[n] = wordlen(p);
+        n++;
+    }
 
-	return n;
+    return n;
 }
 
 void
 pspace(int n)
 {
-	while (n --> 0)
-		putchar(' ');
+    while (n --> 0)
+        putchar(' ');
 }
 
 void
 fmtcols(char *line)
 {
-	char *p;
-	int n;
+    char *p;
+    int n;
 
-	n = 0;
+    n = 0;
 
 #if KEEPINDENT
-	while(isspace(*line))
-		putchar(*line++);
+    while(isspace(*line))
+        putchar(*line++);
 #endif
 
-	for (p = strtok(line, SEP); p != NULL; p = strtok(NULL, SEP)) {
-		fputs(p, stdout);
-		/* don't output unecessary spaces for last column */
-		if (n+1 < ncol)
-			pspace(colsize[n++]-wordlen(p)+nspace);
-	}
+    for (p = strtok(line, SEP); p != NULL; p = strtok(NULL, SEP)) {
+        fputs(p, stdout);
+        /* don't output unecessary spaces for last column */
+        if (n+1 < ncol)
+            pspace(colsize[n++]-wordlen(p)+nspace);
+    }
 
-	putchar('\n');
+    putchar('\n');
 }
 
 int
 help(char *argv0)
 {
-	fprintf(stderr, "%s [-n nspace] [file]\n", argv0);
-	return 1;
+    fprintf(stderr, "%s [-n nspace] [file]\n", argv0);
+    return 1;
 }
 
 int
 main(int argc, char *argv[])
 {
-	char buf[BUFSIZ];
-	FILE *in, *tmp;
-	int ptmp[2];
-	int i, n;
+    char buf[BUFSIZ];
+    FILE *in, *tmp;
+    int ptmp[2];
+    int i, n;
 
-	in = stdin;
+    in = stdin;
 
-	for (i = 1; i < argc; i++) {
-		if (strcmp(argv[i], "-n") == 0) {
-			if (i+1 >= argc)
-				return help(argv[0]);
-			i++;
-			errno = 0;
-			nspace = (int)strtol(argv[i], NULL, 10);
-			if (errno != 0)
-				return help(argv[0]);
-		} else if (strcmp(argv[i], "-h") == 0)
-			return help(argv[0]);
-		else {
-			in = fopen(argv[i], "r");
-			if (in == NULL) {
-				perror(argv[i]);
-				return 1;
-			}
-		}
-	}
-	
-	if (pipe(ptmp) != 0) {
-		perror("pipe");
-		return 1;
-	}
+    for (i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-n") == 0) {
+            if (i+1 >= argc)
+                return help(argv[0]);
+            i++;
+            errno = 0;
+            nspace = (int)strtol(argv[i], NULL, 10);
+            if (errno != 0)
+                return help(argv[0]);
+        } else if (strcmp(argv[i], "-h") == 0)
+            return help(argv[0]);
+        else {
+            in = fopen(argv[i], "r");
+            if (in == NULL) {
+                perror(argv[i]);
+                return 1;
+            }
+        }
+    }
 
-	/* for fgets */
-	tmp = fdopen(ptmp[0], "r");
-	if (tmp == NULL) {
-		perror("fdopen");
-		return 1;
-	}
+    if (pipe(ptmp) != 0) {
+        perror("pipe");
+        return 1;
+    }
 
-	ncol = 0;
+    /* for fgets */
+    tmp = fdopen(ptmp[0], "r");
+    if (tmp == NULL) {
+        perror("fdopen");
+        return 1;
+    }
 
-	/*
-	 * read a first time; tee to pipe for second read.
-	 * get column size.
-	 */
-	for (;;) {
-		memset(buf, '\0', sizeof(buf));
-		if (fgets(buf, sizeof(buf), in) == NULL)
-			break;
-		write(ptmp[1], buf, strlen(buf));
+    ncol = 0;
 
-		n = countcols(buf);
-		if (n > ncol)
-			ncol = n;
-	}
+    /*
+     * read a first time; tee to pipe for second read.
+     * get column size.
+     */
+    for (;;) {
+        memset(buf, '\0', sizeof(buf));
+        if (fgets(buf, sizeof(buf), in) == NULL)
+            break;
+        write(ptmp[1], buf, strlen(buf));
 
-	close(ptmp[1]);
+        n = countcols(buf);
+        if (n > ncol)
+            ncol = n;
+    }
 
-	/*
-	 * read a second time from pipe.
-	 * outputs
-	 */
-	for (;;) {
-		memset(buf, '\0', sizeof(buf));
-		if (fgets(buf, sizeof(buf), tmp) == NULL)
-			break;
+    close(ptmp[1]);
 
-		fmtcols(buf);
-	}
+    /*
+     * read a second time from pipe.
+     * outputs
+     */
+    for (;;) {
+        memset(buf, '\0', sizeof(buf));
+        if (fgets(buf, sizeof(buf), tmp) == NULL)
+            break;
 
-	fclose(tmp); /* will close(ptmp[0]); */
+        fmtcols(buf);
+    }
 
-	if (in != stdin)
-		fclose(in);
+    fclose(tmp); /* will close(ptmp[0]); */
 
-	return 0;
+    if (in != stdin)
+        fclose(in);
+
+    return 0;
 }
